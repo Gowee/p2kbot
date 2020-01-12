ae9b56d71e3b989da247c5584a841894b84fe26a
Hung-I Wang <whygowe@gmail.com>
2020-01-12 19:23:25 +0800

    black format ssmtpd.py

--- ssmtpd.py
+++ ssmtpd.py
@@ -14,21 +14,21 @@ class ExampleHandler:
     #     envelope.rcpt_tos.append(address)
     #     return '250 OK'
     async def handle_DATA(self, server, session, envelope, enable_SMTPUTF8=True):
-        print('Message from %s' % envelope.mail_from)
-        print('Message for %s' % envelope.rcpt_tos)
-        print('Message data:\n')
+        print("Message from %s" % envelope.mail_from)
+        print("Message for %s" % envelope.rcpt_tos)
+        print("Message data:\n")
         with open(f"tmp/{str(int(now() * 1000))}.eml", "wb") as f:
             f.write(envelope.original_content)
-        print('End of message')
-        return '250 Message accepted for delivery'
+        print("End of message")
+        return "250 Message accepted for delivery"
 
 
 async def amain():
-    controller = Controller(ExampleHandler(), hostname='', port=8025)
+    controller = Controller(ExampleHandler(), hostname="", port=8025)
     controller.start()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     loop = asyncio.get_event_loop()
     loop.create_task(amain())
